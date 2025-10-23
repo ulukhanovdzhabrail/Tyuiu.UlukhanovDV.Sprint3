96518f917f0a60571f35e0a5aede6aee5f40dab1
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-10-24 01:31:06 +0500

    Add project files.

---
+++ Tyuiu.UlukhanovDV.Sprint3.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.UlukhanovDV.Sprint3.Task0.V9.Lib
+{
+    public class DataService : ISprint3Task0V9
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+
+            {
+                sumSeries = sumSeries + (Math.Pow(1 / (3 + Math.Pow(value, i)), i));
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.UlukhanovDV.Sprint3.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.UlukhanovDV.Sprint3.Task0.V9.Lib;
+namespace Tyuiu.UlukhanovDV.Sprint3.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 0.430;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
+
